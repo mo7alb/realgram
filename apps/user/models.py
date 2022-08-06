aa0dc198d5c5fab1ccd6c04d507ff0af7c3db407
mo7alb <mo7hammad.alb@gmail.com>
2022-08-06 19:10:40 +0400

    postgres setup and customized user class

---
+++ apps/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+	date_of_birth = models.DateField()
+	bio = models.TextField(max_length=256, null=True)
+	avatar = models.ImageField()
+
+	def __str__(self) -> str:
+		return self.user.username
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'realgram_db',
+        'USER': 'realgram_admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -105,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
