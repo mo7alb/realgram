4a2f7649e80bf72548dc5dbfdbf4006afb6f080b
Mohammad Mahboob <mohammad.bloushi@outlook.com>
2022-08-28 12:03:14 +0400

    added tests for creating new comments

---
+++ apps/comment/tests/new_comments.py
@@ -0,0 +1,104 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from apps.comment.models import Comment
+from apps.post.models import Post
+from apps.user.models import Profile
+
+class NewCommentsTestCase(APITestCase):
+	url = ''
+	data = None
+	profile = None
+	post = None
+
+	def setUp(self) -> None:
+		''' set up variables to be used in tests '''
+		self.url = '/api/comments/' 
+		self.profile = Profile.objects.create(
+			bio="cool guy", 
+			user=User.objects.create(
+				username="johndoe",
+				first_name="john",
+				last_name="johndoe",
+				email="johndoe@somemail.com",
+			)
+		)
+		self.post = Post.objects.create(
+			profile=self.profile,
+			title="welcome to my website"
+		)
+		self.data = {
+			'message': 'Thank you',
+			'post': self.post.pk,
+			'profile': self.profile.id
+		}
+	
+	def tearDown(self) -> None:
+		''' clear db once all tests are completed '''
+		Comment.objects.all().delete()
+		Post.objects.all().delete()
+		Profile.objects.all().delete()
+		User.objects.all().delete()
+
+	def test_adding_new_comment_return_status_code(self) -> None:
+		''' test if creating a new comment returns a status code of 201 '''
+		response = self.client.post(self.url, self.data)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+	def test_adding_new_comment_return_data(self) -> None:
+		''' test if creating a new comment returns the correct message '''
+		response = self.client.post(self.url, self.data)
+		data = response.json()
+		self.assertEqual(data['message'], self.data['message'])
+	
+	def test_adding_comment_with_missing_profile(self) -> None:
+		''' 
+		test if creating a new comment without profile returns a status code of 400 
+		'''
+		response = self.client.post(self.url, {
+			'message' : 'this is a good post',
+			'post': self.post.pk
+		})
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+	
+	def test_adding_comment_with_missing_post(self) -> None:
+		''' 
+		test if creating a new comment without post returns a status code of 400 
+		'''
+		response = self.client.post(self.url, {
+			'message' : 'this is a good post',
+			'profile': self.profile.id
+		})
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+	def test_adding_comment_with_missing_message(self) -> None:
+		''' 
+		test if creating a new comment without message returns a status code of 400 
+		'''
+		response = self.client.post(self.url, {
+			'profile': self.profile.id,
+			'post': self.post.pk
+		})
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+	def test_adding_comment_with_incorrect_post(self) -> None:
+		''' 
+		test if creating a new comment with incorrect post returns a status code of 404
+		'''
+		response = self.client.post(self.url, {
+			'message' : 'this is a good post',
+			'profile': self.profile.id,
+			'post': self.post.pk + 10
+		})
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+	
+	def test_adding_comment_with_incorrect_profile(self) -> None:
+		''' 
+		test if creating a new comment with incorrect profile returns a status code of 404
+		'''
+		response = self.client.post(self.url, {
+			'message' : 'this is a good post',
+			'profile': self.profile.id + 120,
+			'post': self.post.pk
+		})
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
